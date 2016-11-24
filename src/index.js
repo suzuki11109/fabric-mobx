5e4264ce857bdcf65c336ae3736e57b65c28da5f
Suzuki Aki <suzuki11109@gmail.com>
2016-11-24 14:22:20 +0700

    connect fabric state with mobx

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import { observable, autorun } from 'mobx';
+import { fabric } from 'fabric';
 
-var store = observable({
-  name: ""
+let store = observable({
+  canvasObject: {}
 });
 
 autorun(() => {
-  console.log(store.name);
+  if (store.canvasObject.objects) {
+    console.log('top:', store.canvasObject.objects[0].top, ', left:', store.canvasObject.objects[0].left);
+  }
 });
 
-store.name = "Aki";
-store.name = "Akiko";
+let canvas = new fabric.Canvas('canvas', {
+  width: 800, height: 600
+});
+
+canvas.on('object:added', () => {
+  store.canvasObject = canvas.toObject();
+});
+
+canvas.on('mouse:up', () => {
+  store.canvasObject = canvas.toObject();
+});
+
+let rect = new fabric.Rect({
+  top: 100, left: 100, width: 50, height: 50, fill: 'red'
+});
+
+canvas.add(rect);
